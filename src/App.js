d40c45d699ccd50b716316bb7af391d5f08da0ad
lkngtn <luke@lukeduncan.net>
2022-04-11 11:17:16 -0700

    Implement login service

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
+
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
+
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,15 +19,36 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('Logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch ( execption ) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   return (
     <div>
+      {errorMessage && <span>{errorMessage}</span>}
       <h2>blogs</h2>
-      <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} /> 
+      {user === null && 
+        <LoginForm 
+          handleLogin={handleLogin} 
+          username={username} 
+          setUsername={setUsername} 
+          password={password} 
+          setPassword={setPassword} 
+        />} 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
