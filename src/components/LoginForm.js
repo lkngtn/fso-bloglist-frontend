8fc9e8e01b914013eade9de10645a5269deb743c
lkngtn <luke@lukeduncan.net>
2022-04-12 21:56:41 -0700

    Add PropTypes for Togglable and LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({ login }) => {
   const [username, setUsername] = useState('')
@@ -34,6 +35,10 @@ const LoginForm = ({ login }) => {
       <button type="submit">login</button>
     </form>
   )
+}
+
+LoginForm.propTypes = {
+  login: PropTypes.func.isRequired
 }
 
 export default LoginForm
